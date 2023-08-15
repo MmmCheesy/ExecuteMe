45ba71c0cdbd34c09d98f7d7c0396fc9b006b43c
Alex <98720097+MmmCheesy@users.noreply.github.com>
2023-08-15 13:36:38 -0400

    Add files via upload

---
+++ script.py
@@ -0,0 +1,12 @@
+#import web driver 
+import webbrowser as browser
+
+#import audio utilities
+from subprocess import call
+
+#cranks the volume to eleven(100)
+call(["osascript -e 'set volume output volume 100'"], shell=True)
+
+#opens link
+link = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+browser.open(link)
